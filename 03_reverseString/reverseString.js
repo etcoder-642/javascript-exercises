e1fdd1bfd41bbdf3ddb2062b4c2fc897ffcb2563
etcoder-642 <manassehsamuel642@gmail.com>
2025-02-15 05:00:03 -0800

    solved the reverseString challenge

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    string = ''
+    for(i=str.length-1;i>=0;i--){
+        string += str[i]
+    }
+    return string;
 };
 
 // Do not edit below this line
