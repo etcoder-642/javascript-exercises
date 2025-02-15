a05a2f203e329ff8970c57a2a896ddb541b1a13e
etcoder-642 <manassehsamuel642@gmail.com>
2025-02-15 11:50:15 -0800

    solutions for the provided challenges

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rem) {
+    let array = [];
+    for (const arrs of arr){
+        (!(rem.includes(arrs))) ? array.push(arrs): array.push();
+    }
+    return array;
 };
 
 // Do not edit below this line
