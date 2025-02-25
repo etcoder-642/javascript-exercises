b2d9c91b7d98bb2b380ec8b19dfdf45102278995
etcoder-642 <manassehsamuel642@gmail.com>
2025-02-25 22:06:29 +0300

    Solved the problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(value1, value2) {
+	const added = value1 + value2;
+  return added;
 };
 
-const subtract = function() {
-	
+const subtract = function(main, sub) {
+	return main - sub;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length!=0) return arr.reduce((acc, num)=> acc + num)
+    else return 0;	
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, num)=> acc*num)
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+  let pow = num;
+	for(i=0;i<exp-1;i++) num*=pow;
+  return num;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let multiple = 1;
+  for(i=1;i<num+1;i++) 	multiple *= i;
+  return multiple;
 };
 
 // Do not edit below this line
