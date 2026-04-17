e8b1eac9d99f8ad30f2ecde9cad9cb06b39cb0e6
mnasie <manassehsamuel642@gmail.com>
2026-04-17 18:50:19 +0300

    Added some files tried some challenges

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,4 +1,10 @@
-const factorial = function() {
+const factorial = function(num) {
+        if(!Number.isInteger(num) || num < 0) return undefined;
+    if(num == 1 || num == 0){
+        return 1;
+    }else {
+        return num * factorial(num -1);
+    }
 
 };
 
--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,14 @@
-const contains = function() {
-  
+const contains = function(obj, val) {
+    let arr = Object.values(obj);
+    for(let i = 0; i < arr.length; i++){
+        if(Object.prototype.toString.call(arr[i]) == "[object Object]"){
+            if(contains(arr[i], val)) return true;
+        }else {
+            if(Number.isNaN(val) && Number.isNaN(arr[i])) return true;
+            if(arr[i] === val) return true;
+        }
+    }
+    return false;
 };
   
 // Do not edit below this line
